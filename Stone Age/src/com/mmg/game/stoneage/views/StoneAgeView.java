c35e8c5f3f611c39993e977c85d51c81cb7b5c60
Nick <lanikolya1989@gmail.com>
2020-01-08 02:44:22 +0300

    Creation
    Fixed size and background image of StoneAgeView

--- Stone Age/src/com/mmg/game/stoneage/views/StoneAgeView.java
+++ Stone Age/src/com/mmg/game/stoneage/views/StoneAgeView.java
@@ -1,6 +1,7 @@
 package com.mmg.game.stoneage.views;
 
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -12,15 +13,19 @@ import com.mmg.game.stoneage.Application;
 
 public class StoneAgeView extends ViewPart {
 	public static final String ID = "com.mmg.stoneage.views.stoneageview";
-	public static final String FIELD = "/images/field.jpg";
+	public static final String FIELD = "/images/field800.jpg";
 
 	@Override
 	public void createPartControl(Composite parent) {
-		parent.setLayout(new FormLayout());
+//		parent.setLayout(new FormLayout());
+		parent.getParent().getShell().setSize(825,690);
+		Composite field = new Composite(parent, SWT.BORDER);
+		field.setSize(1200,1200);
+//		parent.setSize(1300,900);
 		ImageDescriptor descriptor = AbstractUIPlugin.imageDescriptorFromPlugin(Application.PLUGIN_ID, 
 				FIELD);
 		Image image = descriptor.createImage();
-		parent.setBackgroundImage(image);
+		field.setBackgroundImage(image);
 	}
 
 	@Override
