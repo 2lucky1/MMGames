e1aa8dd866f78cee1341de065736df78388e7f47
Nick <lanikolya1989@gmail.com>
2020-01-13 02:07:52 +0300

    Refactoring
    Displayed app's window on the center of the screen

--- MMGames/src/com/mmg/ApplicationWorkbenchWindowAdvisor.java
+++ MMGames/src/com/mmg/ApplicationWorkbenchWindowAdvisor.java
@@ -1,11 +1,15 @@
 package com.mmg;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.application.ActionBarAdvisor;
 import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
+
 
 public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 
@@ -29,9 +33,15 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 	@Override
 	public void postWindowOpen() {
 		Shell shell = getWindowConfigurer().getWindow().getShell();
-	    shell.setLocation(250, 80);
+	    shell.setLocation(calcInitLocation(shell));
 	    super.postWindowCreate();
 	}
     
-    
+    private Point calcInitLocation(Shell shell) {
+    	Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+    	int x = (int) (screenSize.getWidth()/2-shell.getSize().x/2);
+    	int y = (int) (screenSize.getHeight()/2-shell.getSize().y/2);
+    	return new Point(x,y);
+    }
+	
 }
